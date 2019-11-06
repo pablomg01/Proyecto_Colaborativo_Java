2b1a4e1dd8ad74f60d551cc0a2b8196c17f65170
Pablo Moran <pablomg01@hotmail.com>
2019-11-06 15:19:24 +0100

    Master

--- PabloAdivino/src/pabloadivino/PabloAdivino.java
+++ PabloAdivino/src/pabloadivino/PabloAdivino.java
@@ -12,8 +12,7 @@ public class PabloAdivino {
      */
     public static void main(String[] args) {
     //
-     int n = 0,cont = 0;
-        int x = (int) (500 *Math.random());//guardamos un valor aleatorio entre 0 y 500 no incluido
+     int n = 0,cont = 0;        
         //System.out.print(x);//este system es para ver el numero guardado en x , quitar las dos barras
         // primeras para comprobar
         Scanner sc = new Scanner(System.in);//creamos el objeto sc para utilizar el teclado
