90ff9742b63e679e117411f4039782344b84beb8
Pablo Moran <pablomg01@hotmail.com>
2019-11-07 21:30:56 +0100

    Cambio

--- PabloMG_Adivino/src/pablomg_adivino/PabloMG_Adivino.java
+++ PabloMG_Adivino/src/pablomg_adivino/PabloMG_Adivino.java
@@ -18,7 +18,7 @@ public class PabloMG_Adivino {
      */
     public static void main(String[] args) {
       int n = 0,cont = 0;
-        int x = (int) (500 *Math.random());
+        int x = (int) (Math.random()*(50-10+1)+10);
         System.out.print(x);
         Scanner sc = new Scanner(System.in);
         do {
